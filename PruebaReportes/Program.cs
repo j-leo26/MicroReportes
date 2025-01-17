39f560bfb25df8a5d048b991201eac294910f189
yybeltranr <yybeltranr@unal.edu.co>
2025-01-17 17:45:07 -0500

    Cors activado

--- PruebaReportes/Program.cs
+++ PruebaReportes/Program.cs
@@ -1,32 +1,48 @@
 using Microsoft.EntityFrameworkCore;
 using PruebaReportes.Models;
-
+ 
 var builder = WebApplication.CreateBuilder(args);
-
+ 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+ 
+// Configure Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ 
+// Configure the database context
 builder.Services.AddDbContext<DbAb0bdeTalentseedsContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
-
+ 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", builder =>
+    {
+        builder.WithOrigins("http://localhost:5173") 
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+ 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-//app.UseHttpsRedirection();
-
+ 
+// Enable CORS middleware
+app.UseCors("AllowReactApp");
+ 
+// Uncomment if using HTTPS
+// app.UseHttpsRedirection();
+ 
 app.UseAuthorization();
-
+ 
 app.MapControllers();
-
+ 
 app.Run();
